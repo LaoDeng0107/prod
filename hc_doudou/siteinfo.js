5f47251008e99ad10c1c94e5c44e1529d659be8e
375385089@qq.com <375385089@qq.com>
2018-12-20 14:46:11 +0800

    hc_doudou
    hc_doudou

---
+++ hc_doudou/siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "过三关赢口红",
+    uniacid: "4",
+    acid: "4",
+    multiid: "0",
+    version: "1.1.5",
+  siteroot: "https://ddd.mynatapp.cc/app/index.php",
+    design_method: "3"
+};
